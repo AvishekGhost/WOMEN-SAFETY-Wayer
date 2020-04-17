d90bf10cdceb02391d383a87dfffe34d16b457ca
Avishek Mondal <avishek.mondal53@gmail.com>
2020-04-17 16:43:54 +0530

    Forum style sheet created

--- frontend/src/components/Forum/MyCard/MyCard.js
+++ frontend/src/components/Forum/MyCard/MyCard.js
@@ -1,33 +1,24 @@
 import React from "react";
 import { IonAvatar, IonLabel } from "@ionic/react";
 
+import "./MyCard.css";
+
 const MyCard = ({ name, text, time }) => {
-  return (
-    <div
-      color="primary"
-      style={{
-        maxWidth: "60vw",
-        backgroundColor: "#8ec6c5",
-        display: "flex",
-        flexDirection: "row-reverse",
-        margin: "5px",
-        marginTop: "15px",
-        borderRadius: "15px 15px 0px 15px",
-      }}
-    >
-      <IonAvatar slot="end" style={{ padding: "10px" }}>
-        <img
-          src="https://avatars0.githubusercontent.com/u/33727239?s=400&u=42bad3b9a947f4385ba57e0b59f7f526408eb8f5&v=4"
-          alt="loading"
-        />
-      </IonAvatar>
-      <IonLabel style={{ padding: "10px" }}>
-        <h2>{name}</h2>
-        <h3>{text}</h3>
-        <p>{time}</p>
-      </IonLabel>
-    </div>
-  );
+	return (
+		<div className="my-card-container">
+			<IonAvatar slot="end" style={{ padding: "10px" }}>
+				<img
+					src="https://avatars0.githubusercontent.com/u/33727239?s=400&u=42bad3b9a947f4385ba57e0b59f7f526408eb8f5&v=4"
+					alt="loading"
+				/>
+			</IonAvatar>
+			<IonLabel style={{ padding: "10px" }}>
+				<h3>{name}</h3>
+				<h2>{text}</h2>
+				<p>{time}</p>
+			</IonLabel>
+		</div>
+	);
 };
 
 export default MyCard;
--- frontend/src/components/Forum/OtherCard/OtherCard.js
+++ frontend/src/components/Forum/OtherCard/OtherCard.js
@@ -1,32 +1,24 @@
 import React from "react";
-import { IonAvatar, IonLabel } from "@ionic/react";
+import { IonAvatar, IonLabel, IonTextarea } from "@ionic/react";
+
+import "./OtherCard.css";
 
 const OtherCard = ({ name, text, time }) => {
-  return (
-    <div
-      color="primary"
-      style={{
-        maxWidth: "60vw",
-        backgroundColor: "#fcf7bb",
-        display: "flex",
-        margin: "5px",
-        marginTop: "15px",
-        borderRadius: "15px 15px 15px 0px",
-      }}
-    >
-      <IonAvatar slot="start" style={{ padding: "10px" }}>
-        <img
-          src="https://avatars0.githubusercontent.com/u/33727239?s=400&u=42bad3b9a947f4385ba57e0b59f7f526408eb8f5&v=4"
-          alt="loading"
-        />
-      </IonAvatar>
-      <IonLabel style={{ padding: "10px" }}>
-        <h2>{name}</h2>
-        <h3>{text}</h3>
-        <p>{time}</p>
-      </IonLabel>
-    </div>
-  );
+	return (
+		<div className="other-card-container">
+			<IonAvatar slot="start" style={{ padding: "10px" }}>
+				<img
+					src="https://avatars0.githubusercontent.com/u/33727239?s=400&u=42bad3b9a947f4385ba57e0b59f7f526408eb8f5&v=4"
+					alt="loading"
+				/>
+			</IonAvatar>
+			<IonLabel style={{ padding: "10px" }}>
+				<h3>{name}</h3>
+				<h2>{text}</h2>
+				<p>{time}</p>
+			</IonLabel>
+		</div>
+	);
 };
 
 export default OtherCard;
